345847dae79a1932597828eaeb5693f50148f820
Prerna Dhiman <64417931+Prernn@users.noreply.github.com>
2021-05-10 00:43:40 +0530

    Pattern Problem
    Pattern Problem

---
+++ Problems/reverse-halftriangle.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+    int n; cin>>n;
+    for(int i=n; i>=1; i--)
+    {
+        for(int j=n; j>i; j--)
+        {
+            cout<<"  ";
+        }
+        for(int k=1; k<2*i; k++)
+        {
+            cout<<" *";
+        }
+        cout<<endl;
+    }
+}
+/*Input:- 4
+  Output:-      * * * * * * *
+                  * * * * *
+                    * * *
+                      *     /
